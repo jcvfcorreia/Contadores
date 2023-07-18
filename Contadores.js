c473fe689f9bcf582123388136d80993149020d8
jcvfcorreia <jcvfcorreia@users.noreply.github.com>
2023-07-18 09:53:39 +0100

    Add files via upload

---
+++ Contadores.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#idAgua").click(function(){
+        $("#frame").load("Agua.html");
+    });
+
+    $("#idGas").click(function(){
+        $("#frame").load("Gas.html");
+    });
+
+    $("#idEletricidade").click(function(){
+        $("#frame").load("electricidade.html");
+    });    
+})
+
